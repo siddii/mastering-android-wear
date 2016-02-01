850b8f052e943aabeb4c894da8cee3ee4318018d
Siddique Hameed <siddique_hameed@mastercard.com>
2016-02-01 00:11:45 -0600

    Adding Chapter 3 files

---
+++ Chapter_3/app/src/main/java/com/siddique/androidwear/today/MainActivity.java
@@ -0,0 +1,31 @@
+package com.siddique.androidwear.today;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.wearable.view.WatchViewStub;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends Activity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            @Override
+            public void onLayoutInflated(WatchViewStub stub) {
+                mTextView = (TextView) stub.findViewById(R.id.text);
+
+                Date today = new Date();
+                SimpleDateFormat dateFormat = new SimpleDateFormat("EEEE, MMMM d - yyyy");
+                mTextView.setText("Today is " + dateFormat.format(today));
+            }
+        });
+    }
+}
