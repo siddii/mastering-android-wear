2b94e46eec4c2ebb0b5d4b953de758a046a9bb0f
Siddique Hameed <siddique_hameed@mastercard.com>
2016-03-06 21:45:05 -0600

    Update

--- Chapter_4/app/src/main/java/com/siddique/androidwear/today/DayOfYearActivity.java
+++ Chapter_4/app/src/main/java/com/siddique/androidwear/today/DayOfYearActivity.java
@@ -8,14 +8,14 @@ import java.util.Calendar;
 
 public class DayOfYearActivity extends Activity {
 
-    private TextView mTextView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_day_of_year);
         Calendar calendar = Calendar.getInstance();
-        String dayOfYearDesc = getString(R.string.day_of_year_card_desc, calendar.get(Calendar.DAY_OF_YEAR), calendar.getActualMaximum(Calendar.DAY_OF_YEAR) - calendar.get(Calendar.DAY_OF_YEAR));
+        String dayOfYearDesc = getString(R.string.day_of_year_card_desc,
+                calendar.get(Calendar.DAY_OF_YEAR),
+                calendar.getActualMaximum(Calendar.DAY_OF_YEAR) - calendar.get(Calendar.DAY_OF_YEAR));
         TextView desc = (TextView) findViewById(R.id.day_of_year_desc);
         desc.setText(dayOfYearDesc);
     }
